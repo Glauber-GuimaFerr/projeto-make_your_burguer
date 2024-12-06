b5a6ba7e3415576279bce77ea0b7b2812a240d25
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2024-12-06 08:50:03 -0300

    Adição do status dos pedidos
    Criação de função para o retorno do status dos pedidos criados.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -24,6 +24,7 @@
                 <div>
                     <select name="status" class="status">
                         <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
                     </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>
@@ -47,6 +48,12 @@ export default{
             const req = await fetch('http://localhost:3000/burgers')
             const data = await req.json()
             this.burgers = data
+            this.getStatus()
+        },
+        async getStatus(){
+            const req = await fetch('http://localhost:3000/status')
+            const data = await req.json()
+            this.status = data 
         }
     },
     mounted(){

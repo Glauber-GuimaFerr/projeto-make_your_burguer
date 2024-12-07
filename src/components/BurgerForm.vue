1d0cf5701df5a63bc62666876b006b87343cd518
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2024-12-07 10:30:26 -0300

    Atualização do formulário de pedidos
    Agora o usuário obrigatoriamente necessita digitar o nome, tipo de pão e tipo de carne.

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -10,14 +10,12 @@
                 <div class="input-container">
                     <label for="pao">Escolha o pão:</label>
                     <select name="pao" id="pao" v-model="pao">
-                        <option value="">Selecione o seu pão</option>
                         <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
                     </select>
                 </div>
                 <div class="input-container">
                     <label for="carne">Escolha a carne do seu Burger:</label>
                     <select name="carne" id="carne" v-model="carne">
-                        <option value="">Selecione o tipo de carne</option>
                         <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
                     </select>
                 </div>
@@ -66,26 +64,31 @@ export default{
         },
         async createBurger(e){
             e.preventDefault()
-            const data = {
-                nome: this.nome,
-                carne: this.carne,
-                pao: this.pao,
-                opcionais: Array.from(this.opcionais),
-                status: 'Solicitado' 
-            }
-            const dataJson = JSON.stringify(data)
-            const req = await fetch('http://localhost:3000/burgers', {
-                method: 'POST',
-                headers: {'Content-Type':'application/json'},
-                body: dataJson
-            })
-            const res = await req.json()
-            this.msg = `Pedido N° ${res.id} realizado com sucesso`
-            setTimeout(() => this.msg = "", 5000)
-            this.nome = ''
-            this.carne = ''
-            this.pao = ''
-            this.opcionais = ''
+            if(this.nome != '' && this.pao != '' && this.carne != ''){
+                this.msg = 'Erro! Digite as informações necessárias!'
+                setTimeout(() => this.msg = "", 5000) 
+            }else{
+                const data = {
+                    nome: this.nome,
+                    carne: this.carne,
+                    pao: this.pao,
+                    opcionais: Array.from(this.opcionais),
+                    status: 'Solicitado' 
+                }
+                const dataJson = JSON.stringify(data)
+                const req = await fetch('http://localhost:3000/burgers', {
+                    method: 'POST',
+                    headers: {'Content-Type':'application/json'},
+                    body: dataJson
+                })
+                const res = await req.json()
+                this.msg = `Pedido N° ${res.id} realizado com sucesso`
+                setTimeout(() => this.msg = "", 5000)
+                this.nome = ''
+                this.carne = ''
+                this.pao = ''
+                this.opcionais = ''    
+            } 
         }
     },
     mounted(){
